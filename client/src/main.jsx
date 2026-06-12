822367394079e771eba99a3b9f23da2d802d0f7c
cwoland <tkachenkoa2003@gmail.com>
2026-06-12 08:05:12 +0300

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast';
@@ -20,7 +19,6 @@ style.textContent = `
 document.head.appendChild(style);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
       <Toaster
@@ -39,5 +37,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       }}
       />
     </QueryClientProvider>
-  </React.StrictMode>
 );
